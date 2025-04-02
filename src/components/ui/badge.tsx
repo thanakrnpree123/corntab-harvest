3e24c9ec5b942178714e40b115ad55666ce72662
Tao_IT <thanakarn.123@gmail.com>
2025-04-02 13:16:24 +0700

    fix

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -8,6 +8,7 @@ const badgeVariants = cva(
   {
     variants: {
       variant: {
+        success:"", //WIP
         default:
           "border-transparent bg-primary text-primary-foreground hover:bg-primary/80",
         secondary:
