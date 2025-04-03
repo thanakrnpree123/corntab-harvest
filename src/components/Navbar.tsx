55d5943154770526b191fedae8d7f77d5c42ccb9
Tao_IT <thanakarn.123@gmail.com>
2025-04-03 15:53:28 +0700

    add icon on navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -60,7 +60,7 @@ export function Navbar() {
           </Link>
 
         {/* Desktop menu */}
-        <nav className="hidden md:flex items-center gap-6">
+        <nav className="hidden md:flex items-center mt-[0.45%] ml-[1%]">
           {navLinks.map((link) => (
             <Link
               key={link.name}
